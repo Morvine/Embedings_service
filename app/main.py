8422b38dc875f8af10ce25e0df4e36dfeb672927
Ivan Maksimov <IIMaksimov@greenatom.ru>
2024-03-16 18:37:12 +0300

    fast fix

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import io
 import json
 import os
 import minio
+import pickle
 
 from fastapi import FastAPI, Body, status, HTTPException
 from fastapi.responses import JSONResponse
@@ -25,7 +26,6 @@ model = SentenceTransformer(model_path)
 app = FastAPI()
 client = minio.Minio(
     endpoint=os.getenv('MINIO_ENDPOINT', 'minio:9000'),
-    # endpoint='127.0.0.1:9000',
     access_key=os.getenv('MINIO_ACCESS_KEY', 'cjKMqPAaGfpnsIdRzNZG'),
     secret_key=os.getenv('MINIO_SECRET_KEY', 'WjNFiKfpZAVBDScjhp6w4KzFSy7jRkuB50EhoVl3'),
     secure=False
@@ -59,9 +59,9 @@ def create_embeddings(data=Body()):
 
     data_text = create_text(data, add_speaker=ADD_SPEAKER)
     db = build_index(model, data_text, chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_SIZE, e5_flag=E5_FLAG)
-    if db is not None and db['docs'] is not None and db['embeddings']:
-        value_as_bytes = str(db).encode('utf-8')
-        res_filename = f'{base_path}/' + str(hashlib.md5(value_as_bytes).hexdigest()) + '.json'
+    if db is not None and db['docs'] is not None and db['embeddings'] is not None:
+        value_as_bytes = pickle.dumps(db)
+        res_filename = f'{base_path}/' + str(hashlib.md5(value_as_bytes).hexdigest()) + '.pickle'
 
         res_data = io.BytesIO(value_as_bytes)
 
@@ -85,8 +85,7 @@ def retrieve_docs(data=Body()):
 
     try:
         response = client.get_object(MINIO_BUCKET, minio_path)
-        data = response.data.decode('utf8').replace("'", '"')
-        data = json.loads(data)
+        data = pickle.loads(response.data)
     except ValueError:
         return JSONResponse(
                 status_code=status.HTTP_404_NOT_FOUND,
